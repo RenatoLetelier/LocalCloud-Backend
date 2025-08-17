f24c7fb5b011476727b9a1881c3e080938dfec19
RenatoLetelier <renato.letelier@gmail.com>
2025-08-17 16:30:59 -0400

    creating auth routes

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+import authService from "../services/authService.js";
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !password) {
+    return { message: "Username y password son requeridos" };
+  }
+
+  try {
+    const authSignup = await authService.signup(req.body);
+    res.status(200).json(authSignup);
+  } catch (err) {
+    res.status(500).json({ error: "Error al registrarse" });
+  }
+};
+
+exports.signin = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username y password son requeridos" });
+  }
+
+  try {
+    const authSignin = await authService.signin(req.body);
+    res.status(200).json(authSignin);
+  } catch (err) {
+    res.status(500).json({ error: "Error al iniciar sesión" });
+  }
+};
+
+exports.getProfile = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const profile = await authService.getProfile(userId);
+    res.status(200).json(profile[0]);
+  } catch (err) {
+    res.status(500).json({ error: "Error al iniciar sesión" });
+  }
+};
--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import apiRoutes from "./routes/routes.js";
+import apiRoutes from "./routes.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.BACK_PORT || 3000;
+const FRONT_URL = `${process.env.FRONT_ROOT}:${process.env.FRONT_PORT}`;
+
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: [FRONT_URL], credentials: true }));
 app.use(express.json());
-app.use("/api", apiRoutes);
+app.use(`${process.env.BASE_PATH}`, apiRoutes);
 
 app.get("/", (req, res) => {
   res.send("Home Page! Api working correctly.");
@@ -20,6 +22,6 @@ app.use((req, res) => {
   res.status(404).send("Ups! Page not found.");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT http://localhost:${PORT}`);
 });
