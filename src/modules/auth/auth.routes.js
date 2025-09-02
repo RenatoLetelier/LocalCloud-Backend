1d10198576e1862678de74f0e93b932a1658ab16
Renato Letelier <renatoletelier@Renatos-MacBook-Air.local>
2025-09-02 18:20:11 -0400

    idk

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -8,6 +8,7 @@ const router = Router();
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body ?? {};
+
   if (!email || !password)
     return res.status(400).json({ message: "Email and password required" });
 
